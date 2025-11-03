ec01d4dc95a6228372a76246fb89052dc1691727
sergiuchilat <chilatsergiu@gmail.com>
2025-11-03 12:55:11 +0200

    JS operators

---
+++ examples/js/5. Operators/app.js
@@ -0,0 +1,17 @@
+const a = 2;
+const b = 3.14;
+const c = "hello";
+const d = "5";
+const e = 9;
+
+console.log("a + b =", a + b);           // Addition
+console.log("b - a =", b - a);
+console.log("a * b =", a * b);           // Multiplication
+console.log("b / a =", b / a);
+console.log("b % a =", b % a);           // Modulus
+console.log("e % a =", e % a);           // Modulus
+
+console.log('a + c =', a + c);         // String concatenation
+console.log('c - a =', c - a);
+console.log('a + d =', a + d);         // String concatenation
+console.log('d - a =', d - a);
