dd5d6ccd130de1b4238e1c14eb8d8bd29a5963a8
sergiuchilat <chilatsergiu@gmail.com>
2025-11-03 12:21:46 +0200

    Variables and constants

---
+++ examples/js/2. Variables and constants/app.js
@@ -0,0 +1,19 @@
+var a = 4;
+let b = 5;
+
+
+console.log('a=', a);
+console.log('b=', b);
+
+a = 10;
+b = 15;
+
+console.log('a=', a);
+console.log('b=', b);
+
+
+const c = 20;
+console.log('c=', c);
+
+c = 25; // This will cause an error
+console.log('c=', c);
