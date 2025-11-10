e5142eb458406696728db5e1e3a29d2675925660
sergiuchilat <chilatsergiu@gmail.com>
2025-11-10 10:54:35 +0200

    JS DOMContentLoaded

---
+++ examples/js/7. JS Events - DOMContentLoaded/app.js
@@ -0,0 +1,6 @@
+document.getElementById('image').style.display = 'none';
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('image').style.display = 'block';
+    document.getElementById('loader').style.display = 'none';
+});
